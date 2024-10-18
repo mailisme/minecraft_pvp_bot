dd54f39e393a3256e11111f3f299324c6a776ee5
mailisme <just1213666@gmail.com>
2024-10-18 20:08:35 +0800

    asdf

--- tests/multi_window_control.py
+++ tests/multi_window_control.py
@@ -2,34 +2,17 @@ from win32con import *
 from win32gui import *
 from win32api import *
 import time
-from multi_window_control2 import run
-
 target_hwnds = []
-
 def print_next_title(hwnd, _extra):
     print(GetWindowText(hwnd))
-
 def add_next_hwnd(hwnd, _extra):
     if GetWindowText(hwnd) == "Minecraft 1.8.9":
         EnumChildWindows(hwnd, print_next_title, None)
         target_hwnds.append(hwnd)
-
 EnumWindows(add_next_hwnd, None)
-
-# while True:
-#     for hwnd in target_hwnds:
-#         SetForegroundWindow(hwnd)
-#         keybd_event(0x57, 0, 0, 0)
-#         keybd_event(0x57, 0, 0, KEYEVENTF_KEYUP)
-#         time.sleep(0.05)
-
-def get_hwnd(a):
-    return target_hwnds[a]
-
-run()
-
 while True:
-    SetForegroundWindow(get_hwnd(0))
-    keybd_event(0x57, 0, 0, 0)
-    keybd_event(0x57, 0, 0, KEYEVENTF_KEYUP)
-    time.sleep(0.05)
+    for hwnd in target_hwnds:
+        SetForegroundWindow(hwnd)
+        keybd_event(0x57, 0, 0, 0)
+        keybd_event(0x57, 0, 0, KEYEVENTF_KEYUP)
+        time.sleep(0.05)
